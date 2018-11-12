97a6b1bdba66ae4e7020c19c7a56262a020b1734
Harkishen Singh <hsingh@localhost.localdomain>
2018-11-12 20:16:36 +0530

    connected with mongo and linked analytics

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const app = require('express')(),
     mongo = require('mongodb').MongoClient,
     port = process.env.PORT || 5000,
     host = '0.0.0.0',
-    uri = '',
+    uri = 'mongodb://127.0.0.1:27017',
     bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
@@ -18,11 +18,57 @@ app.get('/', (req, res) => {
 });
 let count = 0;
 app.get('/keys', (req, res) => {
-    let clientObject = JSON.parse(req.query.object);
+    let input = req.query.object;
+    console.warn('Raw \n\n'+input)
+    let clientObject = JSON.parse(input);
     console.warn('Received from client below')
     console.warn(clientObject);
-    res.status(200);
-    res.send('Submitted Successfully!');
+    var conn = mongo.connect(uri, (e, dbo) => {
+        if (e) throw e;
+        console.warn('Connected!');
+        var db = dbo.db('productify_database');
+        db.collection('testing').find({}).toArray((e, result) => {
+            if (e) throw e;
+            else {
+                console.warn('finding collections...');
+                for (let j=0 ; j<clientObject['data'].length; j++) {
+                    let found = false;
+                    for (let i=0 ;i< result.length; i++) {
+                        if ((clientObject['data'][j].word === result[i].word) && ( clientObject['data'][j].tags === result[i].tags ) ) {
+                            found = true;
+                            let updatedObject = {
+                                word: clientObject['data'][j].word,
+                                indepWordWt: result[i].indepWordWt + clientObject['data'][j].indepWordWt,
+                                depWordWt: result[i].depWordWt + clientObject['data'][j].depWordWt,
+                                occurence: result[i].occurence + clientObject['data'][j].occurence,
+                                tags : clientObject['data'][j].tags
+                            };
+                            // db.collection('testing').updateOne(result[i], updatedObject, e => {
+                            //     if (e) 
+                            //         throw e;
+                            //     else 
+                            //         console.warn('Updated Collection for word '+ updatedObject.word);
+                            // })
+                        }
+                    }
+                    if (found === false) {
+                        // here to be written
+                        db.collection('testing').insertOne(clientObject['data'][j], e => {
+                            if (e) 
+                                throw e;
+                            else 
+                                console.warn('inserted collection, since it was not found || word: '+clientObject['data'][j].word);
+                        })
+                    }
+                    
+                } dbo.close();
+                res.status(200);
+                res.send('Submitted Successfully!');
+            }
+        });
+    });
+
+    
     console.log('count :' + ++count)
 });
 
